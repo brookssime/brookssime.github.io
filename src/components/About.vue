099bdace366b276bbce20a78f2326de4b8654bc1
Brooks Sime <brookssime@gmail.com>
2019-01-12 09:55:26 -0600

    added base vue files added less

---
+++ src/components/About.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  
+}
+</script>
+<template>
+  
+</template>
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<template>
+ 
+</template>
+
+<style lang="less" scoped>
+
+</style>
