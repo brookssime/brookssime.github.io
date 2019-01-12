3f7fa14180ae26bfa2c4af7b8c3607088adef37f
Brooks Sime <brookssime@gmail.com>
2019-01-11 22:22:13 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,31 @@
 # abort on errors
 set -e
 
+git checkout development
+
+#install
+npm install
+
 # build
 npm run build
 
 # navigate into the build output directory
 cd dist
 
-
 git add -A
-message='deploy'$(date '+%s')
-git commit -m $message
+
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:brookssime/brookssime.github.io.git master
+git push -f git@github.com:brookssime/brookssime.github.io.git development
 
 
+git checkout master 
+
+git checkout development dist/static dist/index.html
+
+git commit -m 'deploy'
+
+git push -f  git@github.com:brookssime/brookssime.github.io.git master
 
 cd -
